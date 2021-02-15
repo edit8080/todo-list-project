e295a80eae31ec79c2481b3c36671d2dc925a5ca
edit8080 <tlfla01@gmail.com>
2021-02-15 22:27:26 +0900

    Created HTML Element of Task Item

--- javascript/createItem.js
+++ javascript/createItem.js
@@ -5,17 +5,57 @@ const modalBox = document.querySelector(".modal");
 const formBackground = document.querySelector(".bg");
 const inputForm = document.querySelector(".todo-create");
 
-const ID_STORAGE = "idCount";
 const ITEM_STORAGE = "itemStorage"
+const columnName = {
+    "No Status":"no-status",
+    "Next Up":"next-up",
+    "In Progress":"in-progress",
+    "completed":"completed"
+}
 
 let todoItems = [];
 
-// todo : loadItem 을 다른 JS 파일로 설정하여 분할
 
+// 카테고리에 맞게 아이템을 배치 (HTML)
+/*
+    <li class="todo-list__item">
+        <div class="todo-list__item__title">
+            <mark>Todo Title</mark>
+        </div>
+        <div class="todo-list__item__time">
+            2021.02.14 13:00:00
+            <i class="far fa-trash-alt"></i>
+        </div>
+    </li>
+*/
+function setCategoryItem(item){
 
-// todo : 카테고리에 맞게 load한 아이템들 배치
-function setItemCategory(todoItems){
+    // HTML 요소 생성
+    const itemElement = document.createElement("li"),
+        itemTitleArea = document.createElement("div"),
+        itemTitle = document.createElement("mark"),
+        itemTime = document.createElement("div"),
+        itemIcon = document.createElement("i");
 
+    itemElement.classList.add("todo-list__item");
+    itemElement.appendChild(itemTitleArea);
+    itemTitleArea.classList.add("todo-list__item__title");
+    itemTitleArea.appendChild(itemTitle);
+    itemTitle.innerText = item.title;
+    itemTitle.classList.add(`highlight-${item.priority}`);
+
+    itemElement.appendChild(itemTime);
+    itemTime.classList.add("todo-list__item__time");
+    itemTime.innerText = item.date;
+    itemTime.appendChild(itemIcon);
+    itemIcon.classList.add("far");
+    itemIcon.classList.add("fa-trash-alt");
+
+    console.log(columnName[item.category] + `#${columnName[item.category]}`);
+    const itemElements = document.querySelector(`#${columnName[item.category]}`);
+    itemElements.appendChild(itemElement);
+
+    // todo : 수정, 삭제 이벤트 추가
 } 
 
 
@@ -23,10 +63,11 @@ function loadItem(){
     let loadItems = JSON.parse(localStorage.getItem(ITEM_STORAGE));
 
     if(loadItems !== null){
-        for(const loadItem of loadItems)
+        for(const loadItem of loadItems){
             todoItems.push(loadItem);
+            setCategoryItem(loadItem);
+        }   
     }
-    setItemCategory(todoItems);
 }
 
 function init(){
@@ -35,7 +76,7 @@ function init(){
 init();
 
 
-/* 이벤트 */
+/* 생성 이벤트 */
  
 // New 버튼 클릭
 for(const button of createButtons){
@@ -56,7 +97,7 @@ formBackground.addEventListener("click",function(event){
 // save 버튼 클릭(submit)
 inputForm.addEventListener("submit",function(event){  
     event.preventDefault();
- 
+
     const todoItem = {
         "id" : `${new Date().getTime()}`,
         "category" : document.querySelector(".modalBox h2").innerText,
@@ -67,8 +108,8 @@ inputForm.addEventListener("submit",function(event){
     }
     
     todoItems.push(todoItem);
-
     localStorage.setItem(ITEM_STORAGE, JSON.stringify(todoItems));
+    setCategoryItem(todoItem);
 
     inputForm.reset();
     modalBox.classList.add("display-none");   
