1f45dbb1752508857895a8c550a58e645b3d02d5
edit8080 <tlfla01@gmail.com>
2021-02-16 13:15:01 +0900

    Update HTML Element

--- javascript/createItem.js
+++ javascript/createItem.js
@@ -17,43 +17,19 @@ let todoItems = [];
 
 
 // 카테고리에 맞게 아이템을 배치 (HTML)
-/*
-    <li class="todo-list__item">
-        <div class="todo-list__item__title">
-            <mark>Todo Title</mark>
-        </div>
-        <div class="todo-list__item__time">
-            2021.02.14 13:00:00
-            <i class="far fa-trash-alt"></i>
-        </div>
-    </li>
-*/
 function setCategoryItem(item){
-
-    // HTML 요소 생성
-    const itemElement = document.createElement("li"),
-        itemTitleArea = document.createElement("div"),
-        itemTitle = document.createElement("mark"),
-        itemTime = document.createElement("div"),
-        itemIcon = document.createElement("i");
-
-    itemElement.classList.add("todo-list__item");
-    itemElement.appendChild(itemTitleArea);
-    itemTitleArea.classList.add("todo-list__item__title");
-    itemTitleArea.appendChild(itemTitle);
-    itemTitle.innerText = item.title;
-    itemTitle.classList.add(`highlight-${item.priority}`);
-
-    itemElement.appendChild(itemTime);
-    itemTime.classList.add("todo-list__item__time");
-    itemTime.innerText = item.date;
-    itemTime.appendChild(itemIcon);
-    itemIcon.classList.add("far");
-    itemIcon.classList.add("fa-trash-alt");
-
-    console.log(columnName[item.category] + `#${columnName[item.category]}`);
     const itemElements = document.querySelector(`#${columnName[item.category]}`);
-    itemElements.appendChild(itemElement);
+    const itemElement = document.createElement("li");
+    itemElement.classList.add("todo-list__item");
+    itemElement.innerHTML = `         
+            <div class="todo-list__item__title">
+                <mark class="highlight-${item.priority}">${item.title}</mark>
+            </div>
+            <div class="todo-list__item__time">
+                ${item.date}
+            <i class="far fa-trash-alt"></i>
+            </div>`
+    itemElements.appendChild(itemElement);   
 
     // todo : 수정, 삭제 이벤트 추가
 } 
