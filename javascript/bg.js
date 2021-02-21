3169aaa4afdf01fcc2c274a9b3d2ac43bd80a75a
edit8080 <tlfla01@gmail.com>
2021-02-21 15:31:41 +0900

    Created Image Folder

--- javascript/bg.js
+++ javascript/bg.js
@@ -6,7 +6,7 @@ const IMG_NUMBER = 2;
 
 // 배경 이미지를 랜덤으로 지정
 function paintBackground(imgNumber) {
-    todoList.style.backgroundImage = `url('../Todo-background${imgNumber+1}.jpeg')`;
+    todoList.style.backgroundImage = `url('../image/Todo-background${imgNumber+1}.jpeg')`;
 }
 
 function genRandom() {
