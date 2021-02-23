0c93491faabef1d19025c98e6781e7bee1434d32
edit8080 <tlfla01@gmail.com>
2021-02-23 21:52:49 +0900

    Update bg.js

--- javascript/bg.js
+++ javascript/bg.js
@@ -6,7 +6,7 @@ const IMG_NUMBER = 2;
 
 // 배경 이미지를 랜덤으로 지정
 function paintBackground(imgNumber) {
-  todoList.style.backgroundImage = `url('../image/Todo-background${
+  todoList.style.backgroundImage = `url('image/Todo-background${
     imgNumber + 1
   }.jpeg')`;
 }
